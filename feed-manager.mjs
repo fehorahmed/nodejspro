792f6fa6ccc0e72ece5a6ac8c0f7c5d21cafc9d8
ahmedfehor <ahmedfehor@gmail.com>
2025-01-09 01:54:49 +0600

    ok

--- feed-manager.mjs
+++ feed-manager.mjs
@@ -1,16 +1,22 @@
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
-import { access } from "fs/promises";
+import { access, constants, readFile, writeFile } from "fs/promises";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const jsonFile = join(__dirname, 'feeds.json');
 
 // console.log(jsonFile);
-export async function getLinks(){
-    
+export async function getLinks() {
+    try {
+        await access(jsonFile, constants.F_OK);
+    } catch (error) {
+        await writeFile(jsonFile, JSON.stringify([]));
+    }
+    const constents = await readFile(jsonFile, { encoding: 'utf8' });
+    return JSON.parse(constents);
 }
 
-export async function saveLinks(){
-
+export async function saveLinks(links) {
+    await writeFile(jsonFile, JSON.stringify(links));
 }
--- feedreader.mjs
+++ feedreader.mjs
@@ -1,7 +1,57 @@
-import {getLinks, saveLinks} from './feed-manager.mjs';
+import { getLinks, saveLinks } from './feed-manager.mjs';
+import { question, close } from './rl.mjs';
+import https from 'https';
 
 const feeds = await getLinks();
 
-feeds.push('http://www.fehor.com');
+let input = await question('Enter command (list,add,del,read,quit): ');
+
+while (input != 'quit') {
+    let cmdParts = input.trim().split(' ');
+    let cmd = cmdParts[0];
+    //list
+    if (cmd == 'list') {
+        feeds.forEach((url, index) => {
+            console.log(`${index}\t${url}`);
+        });
+    }
+    //Add
+    if (cmd == 'add') {
+        if (cmdParts.length < 2) {
+            console.log('Please include the URL with the add command.');
+        } else {
+            feeds.push(cmdParts[1]);
+        }
+    }
+    //Del
+    if (cmd == 'del') {
+        if (cmdParts.length < 2) {
+            console.log('Please include the Index with the delete command.');
+        } else {
+
+            let index = parseInt(cmdParts[1], 10);
+            if (index > -1 && index < feeds.length) { // only splice array when item is found
+                feeds.splice(index, 1); // 2nd parameter means remove one item only
+            } else {
+                console.log('The provided index is out of range.');
+            }
+        }
+    }
+
+
+    if(cmd == 'read'){
+        https.get('https://www.reddit.com/r/node.rss',()=>{
+        
+        });
+    }
+
+
+
+
+    input = await question('Enter command (list,add,del,read,quit): ');
+}
+
 
 await saveLinks(feeds);
+
+close();
