a11f14c1adc9fea63d459e6ab24fdfa1a756a1c0
ahmedfehor <ahmedfehor@gmail.com>
2025-01-12 01:36:04 +0600

    1.23 minit

--- feedreader.mjs
+++ feedreader.mjs
@@ -1,9 +1,11 @@
 import { getLinks, saveLinks } from './feed-manager.mjs';
 import { question, close } from './rl.mjs';
 import https from 'https';
+import axios from 'axios';
+import Parser from 'rss-parser';
 
 const feeds = await getLinks();
-
+const parser = new Parser();
 let input = await question('Enter command (list,add,del,read,quit): ');
 
 while (input != 'quit') {
@@ -39,10 +41,22 @@ while (input != 'quit') {
     }
 
 
-    if(cmd == 'read'){
-        https.get('https://www.reddit.com/r/node.rss',()=>{
-        
-        });
+    if (cmd == 'read') {
+
+        let {data} = await axios.get('https://www.reddit.com/r/node.rss');
+
+        console.log(data);
+        let feed = await parser.parseString(data);
+        feed.items.forEach(item => console.log(item.title))
+        // https.get('https://www.reddit.com/r/node.rss', (response) => {
+        // let content = '';
+            // response.on('data', (chunk) => {
+            //     content += chunk;
+            // });
+            // response.on('end', () => {
+            //     console.log(content);
+            // });
+        // });
     }
 
 
